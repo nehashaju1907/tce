e3c9fb9f09e954c24fcc7b02fa86749cd1372816
nehashaju1907 <132770281+nehashaju1907@users.noreply.github.com>
2024-05-04 21:25:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Custom JavaScript
+
+$(document).ready(function() {
+    // Add your custom JavaScript here
+    $('#actionButton').click(function() {
+      // Action to perform when the button is clicked
+      alert('ROSE!!!');
+    });
+  });
+  
